3f8590fb0ff09d553eb584787bda38dce96e813a
mingxuan-he <sheldon0527@outlook.com>
2020-12-11 16:45:35 -0600

    Add Gaussian Quadrature Method

--- numerics5_he.py
+++ numerics5_he.py
@@ -54,3 +54,72 @@ def compositeSimpson(function, n, a, b):
 
     return integral
 
+
+def adaptiveSimpson(function, a, b, tolerance=1.0e-12, Sab=0.0, recursion_counter=0, max_depth=20):
+    # function: a function to integrate on the interval [a,b]
+    # a,b: left and right endpoint of the integrating interval
+    # tolerance: tolerance distributed to the specific sub-interval
+    # Sab: Simpson integration value on a current interval [a_i,b_i]
+    # recursion_counter: counts the current recursion level
+
+    # Check inputs
+    if recursion_counter == 0:
+        a = float(a)
+        b = float(b)
+        if a >= b:
+            raise ValueError("[a,b] must be a real interval.")
+    elif recursion_counter > max_depth:
+        return Sab
+
+    # Midpoint
+    c = (a+b)/2
+
+    # Compute simpson's on left and right interval, then sum
+    left_approx = simpson(function,a,c)
+    right_approx = simpson(function,c,b)
+    sum_approx = left_approx + right_approx
+
+    """
+    print("depth: %d" % recursion_counter)
+    print("Sab", Sab)
+    print("sum_approx", sum_approx)
+    print("tolerance", tolerance)
+    """
+    # Compare the left-right sum with previous approximation
+    # If error is small, done.
+    if abs(Sab - sum_approx) <= 10 * tolerance:
+        #print("we're good")
+        return sum_approx
+    else:
+        # If error is large, do next-level recursion and add them up:
+        # interval become [a,c] and [c,b], Sab become the computed approximation on [a,c] and [c,b];
+        # tolerance become half the current tolerance, recursion counter ++
+        #print("going next")
+        return adaptiveSimpson(function,a,c,tolerance=tolerance/2,Sab=left_approx,recursion_counter=recursion_counter+1) + adaptiveSimpson(function,c,b,tolerance=tolerance/2,Sab=right_approx,recursion_counter=recursion_counter+1)
+
+
+# for legendre polynomials: np.polynomial.legendre.leggauss(n)
+
+def gaussQuad(function,n,a,b):
+    # function: a function to integrate on the interval [a,b]
+    # a,b: left and right endpoint of the integrating interval
+    # n: number of sample points taken
+
+    # Check inputs
+    a = float(a)
+    b = float(b)
+    if a >= b:
+        raise ValueError("[a,b] must be a real interval.")
+
+    # change interval
+    if [a,b] != [-1,1]:
+        function = lambda t: function(t * (b-a)/2 + (b+a)/2)
+
+    nodes,coeffs = np.polynomial.legendre.leggauss(n)
+
+    integral = 0
+    for i in range(n):
+        integral += coeffs[i] * function(nodes[i])
+
+    return integral
+
